6a092af390163280b067fe464e85823714618d5d
KarolinaZawisza <zawiszakarolina.t@gmail.com>
2021-09-21 15:54:57 +0200

    Creating a playlist and adding songs

--- main.py
+++ main.py
@@ -1,36 +1,33 @@
+import spotipy
+from spotipy import SpotifyOAuth
 from user_utils import UserUtils
 from data_manager import DataManager
 from spotify_module import SpotifyModule
-from envr import CLIENT_SECRET, CLIENT_ID, URI
-import pprint
-
-pp = pprint.PrettyPrinter(indent=4)
+from envr import CLIENT_SECRET, CLIENT_ID
 
 URL = 'https://www.billboard.com/charts/hot-100'
-date_from_user = UserUtils.get_date_from_user()
+date = UserUtils.get_date_from_user()
 
-website_access = DataManager.scrape_website(URL, date_from_user)
+website_access = DataManager.scrape_website(URL, date)
 soup = DataManager.get_content_from_website(website_access)
 
-
-songs_titles = [title.getText() for title in soup.find_all(name='span', class_='chart-element__information__song'
-                                                                               ' text--truncate color--primary')]
-songs_authors = [title.getText() for title in soup.find_all(name='span', class_='chart-element__information__artist'
-                                                                                ' text--truncate color--secondary')]
-
-spotify = SpotifyModule(CLIENT_ID, CLIENT_SECRET, URI)
-spotify_auth = spotify.authenticate_spotify()
-user_id = spotify_auth.current_user()['id']
+song_names = [title.getText() for title in soup.find_all(name='span',
+                                                         class_='chart-element__information__song'
+                                                                ' text--truncate '
+                                                                'color--primary')]
+spotify_module = SpotifyModule(CLIENT_ID, CLIENT_SECRET, URL)
+sp = spotify_module.authenticate_spotify()
+user_id = sp.current_user()['id']
 
 song_uris = []
-for song in songs_titles:
-    result = spotify_auth.search(q='track:' + song, type='track')
+year = date.split("-")[0]
+for song in song_names:
+    result = sp.search(q=f"track:{song} year:{year}", type='track')
     try:
-        uri = result['tracks']['items'][0]['album']['uri']
-        print(uri)
+        uri = result['tracks']['items'][0]['uri']
         song_uris.append(uri)
     except IndexError:
         print(f"{song} doesn't exist in Spotify. Skipped.")
 
-print(song_uris)
-
+playlist = sp.user_playlist_create(user=user_id, name=f"Musical Time Travel: top 100 from {date}", public=False)
+sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
--- spotify_module.py
+++ spotify_module.py
@@ -8,22 +8,17 @@ class SpotifyModule:
         self.client_id = client_id
         self.client_secret = client_secret
         self.uri = uri
+        self.authenticate_spotify()
 
     def authenticate_spotify(self):
-        sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=self.client_id,
-                                                       client_secret=self.client_secret,
-                                                       redirect_uri=self.uri,
-                                                       scope="playlist-modify-public",
-                                                       show_dialog=True,
-                                                       cache_path="token.txt"
-                                                       ))
+        sp = spotipy.Spotify(
+            auth_manager=SpotifyOAuth(
+                scope="playlist-modify-private",
+                redirect_uri="http://example.com",
+                client_id=self.client_id,
+                client_secret=self.client_secret,
+                show_dialog=True,
+                cache_path="token.txt"
+            )
+        )
         return sp
-
-    @staticmethod
-    def create_playlist(user_id, spotify, date):
-        playlist = spotify.user_playlist_create(user=user_id, name=f"Musical Time Machine: {date}", public=True)
-        return playlist
-
-    @staticmethod
-    def add_tracks_to_playlist(spotify, song_uris, playlist):
-        spotify.playlist_add_items(playlist_id=playlist["id"], items=song_uris, position=None)
