e69ebbbdedbd5207510a464d52a31c22f6c7d87b
ZawiszaKarolina <zawiszakarolina.t@gmail.com>
2022-08-27 14:38:04 +0200

    TR commit

--- user_module.py
+++ user_module.py
@@ -3,5 +3,8 @@ class UserModule:
     def __init__(self, user_id: int):
         self.user_id = user_id
 
-    def get_user_tracks_by_time(self, sorting_type):
-        # 
+    def get_user_tracks_by_time(self):
+        pass
+
+    def sort_user_tracks(self, sorting_type):
+        pass
